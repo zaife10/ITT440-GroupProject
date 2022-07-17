33d1cfb0f28ad4df763e4ca7cdd8ae300cb7691e
zaife10 <2020813046@student/uitm.edu.my>
2022-07-17 19:19:32 +0800

    Adding gui code and tcp code client

---
+++ client.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+import socket
+import threading
+
+window = tk.Tk()
+window.title("Client")
+username = " "
+
+
+topFrame = tk.Frame(window)
+lblName = tk.Label(topFrame, text = "Name:").pack(side=tk.LEFT)
+entName = tk.Entry(topFrame)
+entName.pack(side=tk.LEFT)
+btnConnect = tk.Button(topFrame, text="Connect", command=lambda : connect())
+btnConnect.pack(side=tk.LEFT)
+
+topFrame.pack(side=tk.TOP)
+
+displayFrame = tk.Frame(window)
+scrollBar = tk.Scrollbar(displayFrame)
+scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
+tkDisplay = tk.Text(displayFrame, height=20, width=55)
+tkDisplay.pack(side=tk.LEFT, fill=tk.Y, padx=(5, 0))
+tkDisplay.tag_config("tag_your_message", foreground="blue")
+scrollBar.config(command=tkDisplay.yview)
+tkDisplay.config(yscrollcommand=scrollBar.set, background="#F4F6F7", highlightbackground="grey", state="disabled")
+displayFrame.pack(side=tk.TOP)
+
+
+bottomFrame = tk.Frame(window)
+tkMessage = tk.Text(bottomFrame, height=2, width=55)
+tkMessage.pack(side=tk.LEFT, padx=(5, 13), pady=(5, 10))
+tkMessage.config(highlightbackground="grey", state="disabled")
+tkMessage.bind("<Return>", (lambda event: getChatMessage(tkMessage.get("1.0", tk.END))))
+bottomFrame.pack(side=tk.BOTTOM)
+
+
+def connect():
+    global username, client
+    if len(entName.get()) < 1:
+        tk.messagebox.showerror(title="ERROR!!!", message="You MUST enter your first name ")
+    else:
+        username = entName.get()
+        connect_to_server(username)
+
+
+# network client
+
+HOST_ADDR = "192.168.56.110"
+HOST_PORT = 8080
+
+def connect_to_server(name):
+    global client, HOST_PORT, HOST_ADDR
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST_ADDR, HOST_PORT))
+        client.send(name.encode()) 
+
+        entName.config(state=tk.DISABLED)
+        btnConnect.config(state=tk.DISABLED)
+        tkMessage.config(state=tk.NORMAL)
+
+        # start a thread to keep receiving message from server
+        
+        threading._start_new_thread(receive_message_from_server, (client, "m"))
+    except Exception as e:
+        tk.messagebox.showerror(title="ERROR!!!", message="Cannot connect to host: " + HOST_ADDR + " on port: " + str(HOST_PORT) + " Server may be Unavailable. Try again later")
